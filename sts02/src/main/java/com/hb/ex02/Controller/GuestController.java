69db71db6ac127660dfb427a30c6cf755692ff0a
HB6017\user <chkmkm458.gmail.com>
2016-12-02 14:11:17 +0900

    1. 스프링 sts tool을 통한 crud setting

---
+++ sts02/src/main/java/com/hb/ex02/Controller/GuestController.java
@@ -0,0 +1,43 @@
+package com.hb.ex02.Controller;
+
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hb.ex02.model.GuestDao;
+
+@Controller
+public class GuestController {
+	
+	private GuestDao dao;
+	
+	@Autowired
+	public void setDao(GuestDao dao) {
+		this.dao = dao;
+	}
+	
+	@RequestMapping("/")
+	public String selectAll(Model model){
+		try {
+			model.addAttribute("alist", dao.listAll());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "list";
+	}
+	
+	@RequestMapping("/add")
+	public String addView(){
+		
+		return "addform";
+	}
+	
+	@RequestMapping("/insert")
+	public String insertOne(){
+		dao.insertOne();
+		return "redirect:/";
+	}
+}
